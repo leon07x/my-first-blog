19dc50167f3cf418fa7cad9d47852b99eea5a22d
leon07x <xleo7623@gmail.com>
2018-08-22 17:57:16 +0000

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render, get_object_or_404
 
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
-from django.shortcuts import render, get_object_or_404
 
 from .forms import PostForm
 
